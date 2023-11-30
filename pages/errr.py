323b2f064be31b515025a6f2f217fe4a611706bd
mahoneko <tank89gg@gmail.com>
2023-11-30 15:20:39 +0900

    iiii

--- pages/errr.py
+++ pages/errr.py
@@ -1,8 +1,10 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-from funcs.funcs import show_fx,softmax
+from funcs.funcs import softmax
 import matplotlib.pyplot as plt
+from pyplus.builtin.mathtool import numerical_derivative
+from pyplus.matplotlib.plus import show_fx
 
 def sum_squared_error(y:pd.Series,t:pd.Series)->pd.Series:
     return sum((y-t).apply(lambda x:x**2))/2
@@ -21,42 +23,16 @@ error
 aa=np.random.choice(input_a1['y'],size=2)
 aa
 fig,ax=plt.subplots(1,1)
+ax.set_xlim(-5,5)
+ax.set_ylim(-5,5)
 #show_fx(lambda x:np.sin(x))
 
-def deriv(func,current_x:float):
-    def numerical_diff(func,current_x,delta=1):
-        return (func(current_x+delta)-func(current_x-delta))/(2*delta)
-    def mean_weight(nums,weights):
-        np_nums=np.array(nums)
-        np_weights=np.array(weights)
-        return sum(np_nums*np_weights)/sum(np_weights)
-    
-    dic_res={'slope':[],'weight':[]}
-    delt=4
-    while True:
-        delt = delt/2
-        slope=numerical_diff(func,current_x,delt)
 
-        dic_res['slope'].append(slope)
-        dic_res['weight'].append(1./delt)
-        
-        try:
-            if (diff_slope_pre := dic_res['slope'][-2]-dic_res['slope'][-1])<slope-dic_res['slope'][-1]:
-                st.write('warning')
-        except:
-            pass
 
-        yield mean_weight(dic_res['slope'],dic_res['weight'])
 
 
 inp_slid=st.slider('derivative',-5.,5.,0.,0.01)
-sss=deriv(np.sin,inp_slid)
-
-for ind,val in zip(range(40),sss):
-    #ls=np.linspace(-5.,5.,0.01)
-    #plt.scatter(ls,np.sin(ls))
-    plt.scatter(ind,val)
-
-
-
+show_fx(np.sin)
+aa=numerical_derivative(np.sin,inp_slid)
+show_fx(lambda x:aa*(x-inp_slid)+np.sin(inp_slid))
 st.pyplot(fig)
